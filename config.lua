486b1622747d7fa6e464be8ff3210f021dddc321
s4redmax <tri-rong@hotmail.com>
2015-04-05 23:04:34 +0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+application =
+{
+        content =
+        {
+                width = 1200,
+                height = 1920,
+                scale = "letterbox",
+				xAlign = "left",
+				yAlign = "top",
+				fps = 30,
+				antialias = false,
+        },
+}
